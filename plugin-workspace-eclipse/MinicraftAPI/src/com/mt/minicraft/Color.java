c8b7c3478b87b978f894da32bc086d50f6952d46
Majick Tek <kingtmw2@gmail.com>
2022-11-30 16:21:49 -0500

    cleanup

--- plugin-workspace-eclipse/MinicraftAPI/src/com/mt/minicraft/Color.java
+++ plugin-workspace-eclipse/MinicraftAPI/src/com/mt/minicraft/Color.java
@@ -10,12 +10,12 @@ public enum Color {
 	WHITE(255, 255, 255),
 	BLACK(0, 0, 0);
 	
-	
+	final GenericMath CALCULATOR = GenericMath.Calculator.INSTANCE.get();
 	int r,g,b;
 	Color(int r, int g, int b) {
-		this.r = (int) GenericMath.Calculator.INSTANCE.get().clamp(r, 0, 255);
-		this.b = (int) GenericMath.Calculator.INSTANCE.get().clamp(g, 0, 255);
-		this.g = (int) GenericMath.Calculator.INSTANCE.get().clamp(b, 0, 255);
+		this.r = (int) CALCULATOR.clamp(r, 0, 255);
+		this.b = (int) CALCULATOR.clamp(g, 0, 255);
+		this.g = (int) CALCULATOR.clamp(b, 0, 255);
 	}
 	
 }
