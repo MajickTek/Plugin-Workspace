2fd8c5a1825481da53eea130b91b6f617cd6dd24
Majick Tek <kingtmw2@gmail.com>
2022-11-30 03:57:21 -0500

    add doc to color

--- plugin-workspace-eclipse/OGMinicraft/src/com/mojang/ld22/gfx/Color.java
+++ plugin-workspace-eclipse/OGMinicraft/src/com/mojang/ld22/gfx/Color.java
@@ -2,6 +2,17 @@ package com.mojang.ld22.gfx;
 
 public class Color {
 
+	/**
+	 * Every argument is for one of the grayscales. 
+	 * <br> -1 means transparent.
+	 * <br> any other numbers represent rgb. The first digit is red, the second digit is green, and the third is blue.
+	 * <br> Each digit must be between 0 and 5 
+	 * @param a black
+	 * @param b dark grey
+	 * @param c light grey
+	 * @param d white
+	 * @return
+	 */
 	public static int get(int a, int b, int c, int d) {
 		return (get(d) << 24) + (get(c) << 16) + (get(b) << 8) + (get(a));
 	}
